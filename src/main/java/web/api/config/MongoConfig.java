0ca76b461e3f3cb2a71a38456008b8f7c25d4a6e
namph-uet <hoangnamuet.vnu@gmail.com>
2020-08-29 15:59:33 +0700

    deploy test 6

--- src/main/java/web/api/config/MongoConfig.java
+++ src/main/java/web/api/config/MongoConfig.java
@@ -2,6 +2,7 @@ package web.api.config;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -11,12 +12,13 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories
 public class MongoConfig extends AbstractMongoConfiguration {
 
+    @Value( "${mongo.remoteUri}")
+    private String mongoURi;
 
     @Override
     public MongoClient mongoClient() {
-        MongoClientURI uri = new MongoClientURI("mongodb+srv://web-server:server123@cluster0.tmzfq.mongodb.net/user-service?retryWrites=true&w=majority");
+        MongoClientURI uri = new MongoClientURI(mongoURi);
         return new MongoClient(uri);
-
     }
 
     @Override
